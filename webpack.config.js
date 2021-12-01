45bd1f2f9bf8946ac079682c41fb9dea39a9898a
Priscilla <priscillakim58@gmail.com>
2021-12-01 01:23:07 -0500

    cleaned up folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const path = require("path");
+
 module.exports = {
+  entry: "./client/index.js", //! where your starting file is
+  output: {
+    //! where webpack outputs files
+    path: path.join(__dirname, "public"), // replaces dist folder?? not sure
+    filename: "bundle.js",
+  },
   module: {
     rules: [
       {
