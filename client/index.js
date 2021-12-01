023b3a995c0e128b3faab5ba81dfdecc0b84fae6
Priscilla <priscillakim58@gmail.com>
2021-12-01 01:43:54 -0500

    client side refactor

--- client/index.js
+++ client/index.js
@@ -1,37 +1,5 @@
-import axios from "axios";
 import React, { Component } from "react";
 import { render } from "react-dom";
-//import Users from "./Components/Users";
+import App from "./app";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      loading: true,
-    };
-  }
-  async componentDidMount() {
-    this.setState({
-      users: (await axios.get("/api/users")).data,
-      loading: false,
-    });
-  }
-  render() {
-    const { users, loading } = this.state;
-    if (loading) {
-      return "....loading";
-    }
-    return (
-      <div>
-        <h2>Users({users.length})</h2>
-        <ul>
-          {users.map((user) => {
-            return <li key={user.id}>{user.name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
 render(<App />, document.querySelector("#root"));
